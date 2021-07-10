5f026515ba03eb0e992458b416e934a03ecdd964
jerbear666 <jerryclarkmusic@gmail.com>
2021-07-10 17:35:56 -0500

    Ledge Grab Behavior Added

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/LedgeGrab.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeGrab : MonoBehaviour
+{
+    //handle to hand position
+    [SerializeField]
+    private Vector3 handPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag ("Ledge_Grab_Checker"))
+        {
+            //disable character controller
+
+            //get player component
+            var player = other.transform.parent.GetComponent<Player>();
+            if(player != null)
+            {
+                //call ledge grab method
+                player.LedgeGrab(handPosition);
+            }
+        }
+    }
+}
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
     private Vector3 _direction;
 
     [SerializeField]
-    private bool _canJump;
+    private bool _jumping = false;
 
     private Animator _anim;
 
@@ -30,28 +30,41 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_controller.isGrounded == true)
+        CaluculateMovement();
+     
+    }
+    
+    void CaluculateMovement()
+    {
+        if (_controller.isGrounded == true)
         {
+            if (_jumping == true)
+            {
+                _jumping = false;
+                _anim.SetBool("Jumping", _jumping);
+            }
             //horizontal movement 
             float h = Input.GetAxisRaw("Horizontal"); // using getaxisraw because the float value slowly increments to 1. Raw tells it to go to 1 asap.
             _direction = new Vector3(0, 0, h);//because moving left and right depends on the z axis her
             _anim.SetFloat("Speed", Mathf.Abs(h));
+            
 
-           //FACE CORRECT DIRECTION
-            if(h != 0) // if the z axis doesnt equal 0 
+            //FACE CORRECT DIRECTION
+            if (h != 0) // if the z axis doesnt equal 0 
             {
                 Vector3 facingDirection = transform.localEulerAngles; // variable for facing direction using local Euler Angles
                 facingDirection.y = _direction.z > 0 ? 0 : 180; //facing direction's y position = value of h > 0 else 0 or 180
                 transform.localEulerAngles = facingDirection; //localEulerAngles = the new direction
             }
             //JUMP
-            if(Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 _direction.y = _jumpHeight;
                 //trigger jump anim
-                _anim.SetTrigger("Jump");
+                _jumping = true;
+                _anim.SetBool("Jumping", _jumping);
             }
-          
+
         }
 
         //needs gravity to be grounded
@@ -59,6 +72,16 @@ public class Player : MonoBehaviour
 
         //Move
         _controller.Move(_direction * _speed * Time.deltaTime);
-     
     }
+
+    public void LedgeGrab(Vector3 handPosition)
+    {
+        _controller.enabled = false;
+        _anim.SetBool("LedgeGrab", true);
+        //update position to hand position. 
+        
+        transform.position = handPosition;
+
+    }
+
 }
